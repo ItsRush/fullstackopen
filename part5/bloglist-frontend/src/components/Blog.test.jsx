68e2f1c8ebc87d6f0ebd1c911b384a699a5d4c87
Lejla Hotic <lejlahotic3@gmail.com>
2025-11-08 22:52:39 +0100

    added test for like button and form

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import BlogForm from './BlogForm'
 
 test('renders blog title and author but not URL or likes by default', () => {
   const blog = {
@@ -33,10 +34,9 @@ test('clicking the button shows URL and number of likes', async () => {
     }
 
     const mockLikeBlog = vi.fn()
-    const mockDeleteBlog = vi.fn()
 
     const { container } = render(
-        <Blog blog={blog} likeBlog={mockLikeBlog} deleteBlog={mockDeleteBlog} />
+        <Blog blog={blog} likeBlog={mockLikeBlog}/>
     )
 
 
@@ -49,3 +49,51 @@ test('clicking the button shows URL and number of likes', async () => {
     expect(div).toHaveTextContent('armareforger.com')
     expect(div).toHaveTextContent('likes: 201')
 })
+
+test('clicking the like button twice calls the event handler twice', async () => {
+    const blog = {
+        title: 'Arma Reforger',
+        author: 'Arma',
+        url: 'armareforger.com',
+        likes: 201
+    }
+
+    const mockHandler = vi.fn()
+
+      render (<Blog blog={blog} likeBlog={mockHandler}/>)
+
+    const user = userEvent.setup()
+
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+})
+
+test('<BlogForm /> updates parent state and calls onSubmit', async () => {
+  const createBlog = vi.fn()
+  const user = userEvent.setup()
+
+  render(<BlogForm createBlog={createBlog} />)
+
+  const titleInput = screen.getByLabelText('title:')
+  const authorInput = screen.getByLabelText('author:')
+  const urlInput = screen.getByLabelText('url:')
+  const sendButton = screen.getByText('create')
+
+  await user.type(titleInput, 'testing a form...')
+  await user.type(authorInput, 'Test')
+  await user.type(urlInput, 'http://example.com')
+  await user.click(sendButton)
+
+  expect(createBlog.mock.calls).toHaveLength(1)
+  expect(createBlog.mock.calls[0][0].title).toBe('testing a form...')
+  expect(createBlog.mock.calls[0][0].author).toBe('Test')
+  expect(createBlog.mock.calls[0][0].url).toBe('http://example.com')
+
+  console.log(createBlog.mock.calls)
+} )
