e39cd5abec216a25d25c296b16a034409cbb4853
Lejla Hotic <lejlahotic3@gmail.com>
2026-03-01 20:12:35 +0100

    refactored code

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -45,14 +45,15 @@ const App = () => {
       ]
     }
   ]
-  
-  const courseNumber = courses.map(course => <Course key= {course.id} course={course} />)
-  
+
+
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      {courseNumber}
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}
     </div>
   )
 }
+
 export default App
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,43 +1,42 @@
-const Header = (props) => {
-    return (
-      <div>
-       <h1>{props.name}</h1>
-      </div>
-    )
-  }
-  const Content = (props) => {
-    console.log(props)
-    return (
-      <div>
-        {props.parts.map(part => <Part key={part.id} part={part} />)}
-      </div>
-    )
-  }
-  const Part = (props) => {
-    return (
-      <div>
-        <p>{props.part.name} {props.part.exercises}</p>
-      </div>
-    )
-  }
-  const Total = (props) => {
-    console.log(props)
-    const total = props.parts.reduce((sum, part) => sum+part.exercises,0)
-    return (
-      <div> 
-        <p>Number of exercises {total}</p>
-      </div>
-    )
-  }
-  const Course = (props) => {
-    console.log(props)
-    return (
-      <div>
-        <Header name={props.course.name} />
-        <Content parts={props.course.parts} />
-        <Total parts= {props.course.parts} />
-      </div>
-    )
-  }
+const Header = ({name}) =>{
+  return(
+    <h1>{name}</h1>
+  )
+}
 
-  export default Course
+const Part = ({part}) =>{
+  return(
+    <li>{part.name} {part.exercises}</li>
+  )
+}
+const Content = ({parts}) => {
+  return(
+    <>
+      {parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
+    </>
+  )
+}
+
+const Sum = ({parts}) =>{
+  const total = parts.reduce((sum,part) => {
+    console.log('what is happening', sum, part)
+    return sum+part.exercises
+  }, 0)
+  return(
+    <p>total of {total} exercises</p>
+  )
+}
+const Course = ({course})=>{
+  return(
+    <>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Sum parts={course.parts} />
+    </>
+  )
+}
+
+
+export default Course
